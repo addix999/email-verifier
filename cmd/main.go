343fa7a6951a213999ba4ff4880c045d0b09b765
addix999 <addixvideo@gmail.com>
2025-07-06 16:26:59 +0800

    Add files via upload

---
+++ cmd/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	emailverifier "github.com/AfterShip/email-verifier"
+)
+
+func main() {
+	v := emailverifier.NewVerifier()
+
+	http.HandleFunc("/verify", func(w http.ResponseWriter, r *http.Request) {
+		email := r.URL.Query().Get("email")
+		if email == "" {
+			http.Error(w, "email query param is required", http.StatusBadRequest)
+			return
+		}
+
+		ret, err := v.Verify(email)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(ret)
+	})
+
+	fmt.Println("Server jalan di port 10000...")
+	http.ListenAndServe(":10000", nil)
+}
